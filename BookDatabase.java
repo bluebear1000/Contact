60b26696dbfb3df4102d10998f5bbbc1abede00a
bluebear1000 <43899227+bluebear1000@users.noreply.github.com>
2018-12-08 15:52:48 -0500

    Update BookDatabase.java

--- BookDatabase.java
+++ BookDatabase.java
@@ -1,63 +1,63 @@
 import java.util.*;
 
-public class Book {
-	private String title;
-	private String publisher;
-	private int yearOfPublication;
-	private String[] authors;
-	private Set<String> authorset;
-	private int numAuthors;
+public class BookDatabase {
+	private Map<String, Book> Data;
 
-	Book(String title, String publisher, int yearOfPublication, String[] authors, int numAuthors) {
-		this.title = title;
-		this.publisher = publisher;
-		this.yearOfPublication = yearOfPublication;
-		this.numAuthors = numAuthors;
-		this.authorset = new HashSet<>();
-		this.numAuthors = numAuthors;
-		this.authors = authors;
+	public Book searchByTitle(String title) {
+		return this.Data.get(title);
 	}
 
-	public  String getTitle() {
-		return this.title;
-	}
-
-	public String getPublisher() {
-		return this.publisher;
-	}
-
-	public int getyearOfPublication() {
-		return this.yearOfPublication;
-	}
-
-	public Set<String> getAuthorset() {
-		return this.authorset;
-	}
-	
-	public int numAuthors() {
-		return this.numAuthors;
-	}
-	
-	public int compareTo(Book another) {
-		return this.title.compareTo(another.getTitle());
-	}
-	public boolean equals(Object obj) {
-		return this.title.equals(((Book) obj).getTitle());
-	}
-	
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-			sb.append("Title: " + this.title);
-			sb.append("Authors: ");
-			for (int i = 0; i < authors.length; i++) {
-				sb.append(i < authors.length-1 ? authors[i] : "\n");
+	public Set<Book> searchByAuthor(String author) {
+		Set<Book> tempSet = new HashSet<Book>();
+		for (Map.Entry<String, Book> entry : Data.entrySet()) {
+			if (entry.getValue().getAuthorset().contains(author)) {
+				tempSet.add(entry.getValue());
 			}
-			
-			sb.append("Publisher: " + this.publisher );
-			sb.append("Year: " + this.yearOfPublication);
-			
-			return sb.toString();
-			
+		}
+		return tempSet;
+	}
+
+	public Set<Book> searchP(String publisher) {
+		Set<Book> temp = new HashSet<>();
+		for (Map.Entry<String, Book> entry : Data.entrySet()) {
+			if (entry.getValue().getPublisher().equals(publisher)) {
+				temp.add(entry.getValue());
+			}
+		}
+		return temp;
+	}
+
+	public Set<Book> publishedBefore(int year) {
+		Set<Book> temp = new HashSet<>();
+		for (Map.Entry<String, Book> entry : Data.entrySet()) {
+			if (entry.getValue().getyearOfPublication() < year) {
+				temp.add(entry.getValue());
+			}
+		}
+		return temp;
+	}
+
+	public void remove(String title) {
+		if (this.Data.containsKey(title)) {
+			this.Data.remove(title);
 		}
 	}
+
+	public void add(Book add) {
+		if (!(this.Data.containsKey(add.getTitle()))) {
+			this.Data.put(add.getTitle(),add);
+		
+		}
+	}
+	
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		for (String s : Data.keySet()) {
+			sb.append(Data.get(s));
+		}
+		return sb.toString();
+	}
+
+}
 
